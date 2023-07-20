7471e86fb3e743d0d1340ac37d7a0035663c4ee2
AndriusCicirka <andriusc@wix.com>
2023-07-20 12:22:39 +0300

    Added Event class

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const calendarTable = document.querySelector('.calendar-table');
 const eventModal = document.querySelector('.event-creation--modal');
 const eventModalCloseBtn = document.querySelector('.modal-close--button');
 
+const eventBlobs = document.querySelectorAll('.calendar-event');
+
 class Modal {
 	constructor(modal, closeBtn, callbacks) {
 		this.modal = modal;
@@ -12,20 +14,26 @@ class Modal {
 
 		this.closeBtn.addEventListener('click', () => this.hide());
 	}
-	open() {
+	open(eventBlobId) {
 		this.modal.classList.remove('hide');
 	}
 	hide() {
 		this.modal.classList.add('hide');
 	}
 	toggle() {
-		this.modal.classList.toggle('hide');
+		//this.modal.classList.toggle('hide');
 	}
 }
 
 class Calendar {
 	constructor(table, callbacks) {
 		this.table = table.addEventListener('click', callbacks.onClick);
+	}
+}
+
+class Event {
+	constructor(eventBlobs, callbacks) {
+		this.eventBlobs = eventBlobs;
 	}
 }
 
@@ -36,3 +44,5 @@ const calendar = new Calendar(calendarTable, {
 		modal.toggle(eventModal);
 	},
 });
+
+const event = new Event(eventBlobs, {});
