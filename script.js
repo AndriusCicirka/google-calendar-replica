40911ff4eab29539546ad92e7853f389ef3c7011
AndriusCicirka <andriusc@wix.com>
2023-07-20 10:12:39 +0300

    Added modal and calendar classes

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 'use strict';
 
+const calendarTableWrap = document.querySelector('.calendar-table--wrap');
+const calendarTableEvent = document.querySelector('.calendar-table');
+
+const eventModal = document.querySelector('.event-creation--modal');
+
 class Modal {
-	constructor() {
-		const eventModal = document.querySelector('.event-creation--modal');
+	constructor(callbacks) {}
+	open() {
+		eventModal.classList.remove('hide');
+	}
+	hide() {
+		eventModal.classList.add('hide');
+	}
+	toggle() {
+		eventModal.classList.toggle('hide');
 	}
 }
+
+class Calendar {
+	constructor(callbacks) {
+		calendarTableWrap.addEventListener('click', callbacks.onClick);
+	}
+}
+
+const modal = new Modal();
+
+const calendar = new Calendar({
+	onClick() {
+		modal.toggle();
+	},
+});
