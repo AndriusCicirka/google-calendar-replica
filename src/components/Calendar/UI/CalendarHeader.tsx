afed0a92781ec7c1a6f256fa4a3e16e5f1a7a45c
AndriusCicirka <andriusc@wix.com>
2023-08-10 10:48:56 +0300

    Added basic key generation for dynamically loaded cells

--- src/components/Calendar/UI/CalendarHeader.tsx
+++ src/components/Calendar/UI/CalendarHeader.tsx
@@ -23,9 +23,9 @@ const loadHeaderCells = (weekdays: string[]): JSX.Element[] => {
 	//TO-DO: Make component accept date objects instead of plain numbers
 	//TO-DO: Add check if the displayed week has a day of today's date
 	if (weekdays.length === 7) {
-		return weekdays.map((day): JSX.Element => {
+		return weekdays.map((day, index): JSX.Element => {
 			return (
-				<div className="calendar-header--cell">
+				<div className="calendar-header--cell" key={index + 123}>
 					<span className="calendar-header--cell--letters">SUN</span>
 					<span className="calendar-header--cell--number">{day}</span>
 				</div>
