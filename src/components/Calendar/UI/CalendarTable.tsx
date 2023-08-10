e99281d5ce88b82dd066ac14d7e2438d53fc1b41
AndriusCicirka <andriusc@wix.com>
2023-08-10 13:00:59 +0300

    Refactored generateTableCells to also generate time markings

--- src/components/Calendar/UI/CalendarTable.tsx
+++ src/components/Calendar/UI/CalendarTable.tsx
@@ -8,52 +8,76 @@ interface Props {
 const WEEK_LENGTH = 7;
 const HOURS_IN_DAY = 24;
 
-const generateTableCells = (
-	weekLength: number,
-	hoursInDay: number
-): JSX.Element[] => {
-	const tableCells: JSX.Element[] = [];
+const TIME_MARKINGS = [
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'10',
+	'11',
+	'12',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'10',
+	'11',
+];
 
-	for (let i = 1; i < weekLength * hoursInDay; i++) {
-		tableCells.push(<div className="calendar-table--cell" key={i}></div>);
+const calculatePreffix = (i): 'AM' | 'PM' => {
+	if (i < 11) {
+		return 'AM';
 	}
 
-	return tableCells;
+	return 'PM';
 };
 
-const CalendarTable: React.FC<Props> = () => {
+const generateCells = (
+	timeMarkings?: string[],
+	preffixAmPm?: boolean
+): JSX.Element[] => {
+	const cells: JSX.Element[] = [];
+
+	if (!timeMarkings) {
+		for (let i = 1; i < WEEK_LENGTH * HOURS_IN_DAY; i++) {
+			cells.push(<div className="calendar-table--cell" key={i}></div>);
+		}
+	} else if (timeMarkings) {
+		for (let i = 0; i < HOURS_IN_DAY - 1; i++) {
+			cells.push(
+				<span className="time-marker" key={i}>
+					{timeMarkings[i]} {preffixAmPm && calculatePreffix(i)}
+				</span>
+			);
+		}
+	} else {
+		throw new Error(
+			'CalendarTable encountered an error while generating cells'
+		);
+	}
+
+	return cells;
+};
+
+const CalendarTable: React.FC<Props> = (): JSX.Element => {
 	return (
 		<div className="calendar-wrap">
 			<div aria-hidden="true" className="calendar-table--time">
-				<span className="time-marker">1 AM</span>
-				<span className="time-marker">2 AM</span>
-				<span className="time-marker">3 AM</span>
-				<span className="time-marker">4 AM</span>
-				<span className="time-marker">5 AM</span>
-				<span className="time-marker">6 AM</span>
-				<span className="time-marker">7 AM</span>
-				<span className="time-marker">8 AM</span>
-				<span className="time-marker">9 AM</span>
-				<span className="time-marker">10 AM</span>
-				<span className="time-marker">11 AM</span>
-				<span className="time-marker">12 PM</span>
-				<span className="time-marker">1 PM</span>
-				<span className="time-marker">2 PM</span>
-				<span className="time-marker">3 PM</span>
-				<span className="time-marker">4 PM</span>
-				<span className="time-marker">5 PM</span>
-				<span className="time-marker">6 PM</span>
-				<span className="time-marker">7 PM</span>
-				<span className="time-marker">8 PM</span>
-				<span className="time-marker">9 PM</span>
-				<span className="time-marker">10 PM</span>
-				<span className="time-marker">11 PM</span>
+				{generateCells(TIME_MARKINGS, true)}
 			</div>
 			<div className="calendar-table--wrap">
 				<div className="calendar-table"></div>
-				<div className="calendar-table--invisible">
-					{generateTableCells(WEEK_LENGTH, HOURS_IN_DAY)}
-				</div>
+				<div className="calendar-table--invisible">{generateCells()}</div>
 			</div>
 		</div>
 	);
