a90211046106c4569a0a98c08f38b6489f12920a
AndriusCicirka <andriusc@wix.com>
2023-08-18 14:10:04 +0300

    Added helper function for reusable interfaces

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,22 @@
+export interface EventInfo {
+	id: string;
+	key: string;
+	title: string;
+	description?: string;
+	startingDate: Date;
+	finishingDate: Date;
+	startingDateId?: string;
+	finishingDateId?: string;
+	overlapping: boolean;
+	locale?: string;
+}
+
+export interface StyledEventInfo extends EventInfo {
+	width?: string;
+	gridRow: string;
+	gridColumn: string;
+	marginBottom?: string;
+	marginTop?: string;
+	paddingBottom?: string;
+	paddingTop?: string;
+}
